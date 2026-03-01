09245879da33965f8d924436e678035ea4550a7d
somnath <somnath2004dhere@gmail.com>
2026-03-01 16:55:25 +0530

    add env file

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL="https://qna-backend-2-dpth.onrender.com/api/qna/ask";
+const API_URL=import.meta.env.VITE_API_URL;
 
 export const fetchChatResponse =async (question) => {
 
